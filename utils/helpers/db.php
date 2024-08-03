342e2d706ebcc4602357b03907f04af9d91199a4
bes <besufikadtilahun1994@gmail.com>
2024-08-03 15:29:22 +0300

    dotenv added

--- utils/helpers/db.php
+++ utils/helpers/db.php
@@ -1,17 +1,27 @@
 <?php
-
 namespace Utils\Helpers;
+
+require_once __DIR__ . "/dotenv.php";
 
 use PDO;
 use PDOException;
+use Utils\Helpers\DotEnvEnvironment;
 
 class DBConnection {
-
     private $pdo;
 
     public function __construct(){
         try {
-            $this->pdo = new PDO("mysql:host=127.0.0.1;dbname=rest_demo", "root", "");
+            (new DotEnvEnvironment())->load(__DIR__ . "/../../");
+
+            $host = $_ENV["DB_HOST"];
+            $dbname = $_ENV["DB_NAME"];
+            $user = $_ENV["DB_USER"];
+            $password = $_ENV["DB_PASSWORD"] != "" ? $_ENV["DB_PASSWORD"] : "";
+            $url = "mysql:host=" . $host . ";dbname=" . $dbname;
+            var_dump($password);
+
+            $this->pdo = new PDO($url, $user, $password);
         } catch (PDOException $e) {
             die($e->getMessage());
         }
